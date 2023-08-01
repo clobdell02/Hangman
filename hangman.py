a537a01665a1faa7b31242fd5972039f50b06b4e
Colton Lobdell <83106043+clobdell02@users.noreply.github.com>
2023-07-31 18:31:34 -0700

    Update hangman.py
    still working on fixing the random word not always being selected

--- hangman.py
+++ hangman.py
@@ -12,75 +12,88 @@ food = '''pasta hamburgers hotdogs cereal toast steak chicken apples
 videoGames = '''halo cod bioshock pokemon valorant mario destiny titanfall 
                 madden minecraft roblox skyrim starfield'''
 
-##def chose_a_category():
-category = str(input('What category would you like: sports, food, or games? '))
-if category == 'sports':
-    randomSport = sports.split(' ')
-    word = random.choice(randomSport)
-elif category == 'food':
-    randomFood = food.split(' ')
-    word = random.choice(randomFood)
-elif category == 'games':
-    randomGame = videoGames.split(' ')
-    word = random.choice(randomGame)
-else:
-    print('Please enter a listed category')
-    chose_a_category()
+def chose_a_category():
+    category = str(input('What category would you like: sports, food, or games? '))
+    if category == 'sports':
+        print("You have selected Sports")
+        randomSport = sports.split(' ')
+        word = random.choice(randomSport)
+    elif category == 'food':
+        print("You have selected Food")
+        randomFood = food.split(' ')
+        word = random.choice(randomFood)
+    elif category == 'games':
+        print("You have selected Video Games")
+        randomGame = videoGames.split(' ')
+        word = random.choice(randomGame)
+    else:
+        print('Please enter a listed category')
+        chose_a_category()
+    return word
 
-if __name__ == '__main__':
-    ##word = chose_a_category()
-
-    for letter in word:
-        print('_', end='_')
-    print()
-
-    playing = True
+def gamePlay():
+    word = chose_a_category()
     letter_guessed = ''
+    prior_guesses = []
     chances = len(word) + 2
     correct = 0
     gamestate = 0 ##updated to 1 when the game is won
-    try:
-        while (chances != 0) and gamestate == 0:
-            print()
-            chances -= 1
 
-            try:
-                guess = str(input('Enter a letter: '))
-            except:
-                print('Please only use letters.')
-                continue
+    for letter in word:
+        print('_', end=' ')
+    print()
 
-            if not guess.isalpha():
-                print('Please guess a letter')
-                continue
-            elif (len(guess) > 1):
-                print('Please only guess one letter')
-                continue
-
-            if guess in word:
-                guess_occurence = word.count(guess)
-                for i in range (guess_occurence):
-                    letter_guessed += guess
-            
-            for char in word:
-                if char in letter_guessed and (Counter(letter_guessed) != Counter(word)):
-                    print(char, end=' ')
-                    correct += 1
-                elif (Counter(letter_guessed) == Counter(word)):
-                    print(word, end=' ')
-                    gamestate = 1
-                    print('WooHoo! You have won the game!')
-                    break
-                    break
-                else:
-                    print('_', end=' ')
-        
-        if (chances <= 0) and (Counter(letter_guessed) != Counter(word)):
-            print()
-            print('Sadly you have been defeated')
-            print('The correct answer was {}'.format(word))
-    
-    except KeyboardInterrupt:
+    while (chances != 0) and gamestate == 0:
         print()
-        print('Sorry you have to go, Have a good day.')
-        exit()
+        print('You have {}'.format(chances), ' guesses remaining.')
+        chances -= 1
+        try:
+            guess = str(input('Enter your guess: '))
+        except:
+            print('Please only use letters.')
+            continue
+
+        ## check for a proper guess, must be an alphabetic character with length of 1
+        if not guess.isalpha():
+            print('Please guess an alphabertic character')
+            continue
+        elif (len(guess) > 1):
+            print('Please only guess one letter')
+            continue
+        elif guess in prior_guesses:
+            print('You have already guessed this letter')
+            #chances += 1 ##uncomment for easier gameplay 
+            continue
+        else:
+            prior_guesses.append(guess)
+        
+        ## what to do if user's guess was correct
+        if guess in word:
+            guess_occurence = word.count(guess)
+            for i in range (guess_occurence):
+                letter_guessed += guess
+        
+        ## printing the updated gamestate (the word with the correctly guessed letter filled in)
+        for char in word:
+            if char in letter_guessed and (Counter(letter_guessed) != Counter(word)):
+                print(char, end=' ')
+                correct += 1
+            elif (Counter(letter_guessed) == Counter(word)):
+                print()
+                print(word, end=' ')
+                gamestate = 1
+                print('WooHoo! You have won the game!')
+                break
+                break
+            else:
+                print('_', end=' ')
+    
+    ## if the user has used all their guesses
+    if (chances <= 0) and (Counter(letter_guessed) != Counter(word)):
+        print()
+        print('Sadly you have been defeated')
+        print('The correct answer was {}'.format(word))
+
+
+if __name__ == '__main__':
+    gamePlay()
